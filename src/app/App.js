95f9880fbe8e6af82d862fae42282305204a40fe
Adam Wieckert <wiec1369@gmail.com>
2018-06-25 21:01:25 -0500

    Setup is done

--- src/app/App.js
+++ src/app/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p>{2 + 2}</p>
+        <button className="btn btn-danger">ima button</button>
       </div>
     );
   }
